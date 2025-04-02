a4571f9c49a228098435b0db1c1d8b2223340b92
Tamako Mori <tamako@tamakon.jp>
2025-04-02 20:15:37 +0000

    Fixed tests/setup.sh

--- tests/setup.sh
+++ tests/setup.sh
@@ -3,13 +3,12 @@
 echo "Installing..."
 
 sudo apt-get install -y \
-     qemu-user \
-     qemu-user-binfmt \
+     qemu-user-static \
      crossbuild-essential-i386 \
      crossbuild-essential-amd64 \
      crossbuild-essential-powerpc \
      crossbuild-essential-ppc64el \
-     crossbuild-essential-armel \
+     crossbuild-essential-armhf \
      crossbuild-essential-arm64 \
      crossbuild-essential-mips \
      crossbuild-essential-mips64
